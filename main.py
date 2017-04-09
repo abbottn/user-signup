d456413e035d18358790b86e752a6087f5dfb0f3
Nick Abbott <Nicholas.Abbott.BSEE@gmail.com>
2017-04-09 04:01:39 -0500

    first stopping point: starter code after initial setup

--- main.py
+++ main.py
@@ -15,11 +15,65 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
 
-class MainHandler(webapp2.RequestHandler):
+# html boilerplate for the top of every page
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Signup</title>
+</head>
+<body>
+    <h1>Signup</h1>
+"""
+
+# html boilerplate for the bottom of every page
+page_footer = """
+</body>
+</html>
+"""
+
+
+class Index(webapp2.RequestHandler):
+    """ Handles requests coming in to '/' (the root of our site)
+        e.g. www.user-signup.com/
+    """
+
     def get(self):
-        self.response.write('Hello world!')
+        form = """
+        <form action="/welcome" method="get">
+            <table>
+                <tr><td>Username</td><td><input type="text" name="username"/></td></tr>
+                <tr><td>Password</td><td><input type="text" name="password"/></td></tr>
+                <tr><td>Verify Password</td><td><input type="text" name="verify"/></td></tr>
+                <tr><td>Email (optional)</td><td><input type="text" name="email"/></td></tr>
+            </table>
+            <input type="submit" value="Submit"/>
+        </form>
+        """
+
+        content = page_header + form + page_footer
+        self.response.write(content)
+
+class Welcome(webapp2.RequestHandler):
+    """ Handles requests coming in to '/welcome'
+        e.g. www.user-signup.com/welcome
+    """
+
+    def get(self):
+        # look inside the request to figure out what the user typed
+        user_name = self.request.get("username")
+
+        user_name = cgi.escape(user_name, quote=True)
+
+        # build response content
+        sentence = "<strong>Welcome, " + user_name + "</strong>"
+
+        content = page_header + "<p>" + sentence + "</p>" + page_footer
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Index),
+    ('/welcome', Welcome)
 ], debug=True)
